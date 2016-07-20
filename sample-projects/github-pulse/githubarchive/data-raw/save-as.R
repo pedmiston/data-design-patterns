1163326316f5fef091a070374e7669bd8264cf44
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-07-20 09:56:02 -0500

    Moved github-pulse to its own repo

--- sample-projects/github-pulse/githubarchive/data-raw/save-as.R
+++
@@ -1,8 +0,0 @@
-library(devtools)
-library(dplyr)
-library(readr)
-
-csvs <- list.files("data-raw", pattern = "*.csv", full.names = TRUE)
-activity <- read_csv(csvs)
-
-use_data(activity, overwrite = TRUE)
